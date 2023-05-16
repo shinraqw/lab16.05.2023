51952a7d342cd85a8954fed2d50b98b6158b8661
shinraqw <oliynik05vitaliy@gmail.com>
2023-05-16 19:49:07 +0300

    Add project files.

---
+++ 16.05.2023/Class1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16._05._2023
+{
+
+        public class Film : IDisposable//1
+        {
+            private bool disposed = false;
+
+            public string Title { get; set; }
+            public string Studio { get; set; }
+            public string Genre { get; set; }
+            public int Duration { get; set; }
+            public int Year { get; set; }
+
+            public Film(string title, string studio, string genre, int duration, int year)
+            {
+                Title = title;
+                Studio = studio;
+                Genre = genre;
+                Duration = duration;
+                Year = year;
+            }
+
+            public void Play()
+            {
+                Console.WriteLine("Зараз грає: " + Title);
+            }
+
+            public void Dispose()
+            {
+                Dispose(true);
+            }
+
+            protected virtual void Dispose(bool disposing)
+            {
+                if (!disposed)
+                {
+                    if (disposing)
+                    {
+                        // Звільняємо керовані ресурси (якщо є)
+                    }
+
+                    // Звільняємо некеровані ресурси (якщо є)
+                    disposed = true;
+                }
+            }
+
+            public override string ToString()
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.AppendLine($"Title: {Title}");
+                sb.AppendLine($"Studio: {Studio}");
+                sb.AppendLine($"Genre: {Genre}");
+                sb.AppendLine($"Duration: {Duration} min");
+                sb.AppendLine($"Year: {Year}");
+                return sb.ToString();
+            }
+
+            ~Film()
+            {
+                Dispose(false);
+            }
+        }
+
+        public class Performance : IDisposable//2
+        {
+            private bool disposed = false;
+
+            public string Title { get; set; }
+            public string Theatre { get; set; }
+            public string Genre { get; set; }
+            public int Duration { get; set; }
+            public List<string> Actors { get; set; }
+            public List<string> Audience { get; set; }
+            public List<string> LeadActors { get; set; }
+
+            public Performance(string title, string theatre, string genre, int duration, List<string> actors, List<string> leadActors)
+            {
+                Title = title;
+                Theatre = theatre;
+                Genre = genre;
+                Duration = duration;
+                Actors = actors;
+                Audience = new List<string>();
+                LeadActors = leadActors;
+            }
+
+            public void Start()
+            {
+                Console.WriteLine($"The performance '{Title}' is starting at '{Theatre}'.");
+            }
+
+            public void AddAudienceMember(string name)
+            {
+                Audience.Add(name);
+                Console.WriteLine($"{name} is attending the performance '{Title}'.");
+            }
+
+            public void Dispose()
+            {
+                Dispose(true);
+                GC.SuppressFinalize(this);
+            }
+
+            protected virtual void Dispose(bool disposing)
+            {
+                if (!disposed)
+                {
+                    if (disposing)
+                    {
+                        // Звільняємо керовані ресурси (якщо є)
+                    }
+
+                    // Звільняємо некеровані ресурси (якщо є)
+                    disposed = true;
+                }
+            }
+
+            public override string ToString()
+            {
+                return $"Title: {Title}\nTheatre: {Theatre}\nGenre: {Genre}\nDuration: {Duration} minutes\nActors: {string.Join(", ", Actors)}\nLead Actors: {string.Join(", ", LeadActors)}\nAudience: {string.Join(", ", Audience)}";
+            }
+
+            ~Performance()
+            {
+                Dispose(false);
+            }
+        }
+}
+
---
+++ 16.05.2023/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _16._05._2023
+{
+    class Program
+    {
+        static void Main()
+        {
+            using (var film = new Film("Початок", "Warner Bros.", "Трилер", 148, 2010))//1
+            {
+                film.Play();
+                Console.WriteLine(film);
+            }
+            var actors = new List<string> { "Actor 1", "Actor 2", "Actor 3" };//2
+            var leadActors = new List<string> { "Lead Actor 1", "Lead Actor 2" };
+
+            using (var performance = new Performance("Hamlet", "Shakespeare Theatre", "Tragedy", 180, actors, leadActors))
+            {
+                performance.Start();
+                performance.AddAudienceMember("John");
+                performance.AddAudienceMember("Alice");
+                Console.WriteLine(performance);
+                // Додатковий код для роботи з об'єктом "performance"
+            }
+
+        }
+    }
+}
